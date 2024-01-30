e39114cdb75e4b41054d07b0d0121e9f8df8cdda
Aakash Verma <aakashverma1262@gmail.com>
2024-01-30 23:59:36 +0530

    add copy right in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <footer className="container-fluid text-center" style={{ backgroundColor: 'black', color: 'white', padding: '20px' }}>
       {/* Uncomment the line below if you want to include a copyright statement */}
-      {/* <p>&copy; 2024 Prerna Morwal. All Rights Reserved.</p> */}
+       <p>&copy; 2024 Aakash Verma. All Rights Reserved.</p>
       <p>Contact me at: <a href="mailto:aakashverma1262@gmail.com" style={{color:"white", textDecoration:"none"}}>aakashverma1262@gmail.com</a></p>
     </footer>
   );
